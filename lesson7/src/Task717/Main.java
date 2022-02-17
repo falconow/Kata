048e6aece0915596e2450436f4229110217eb81b
Dmitry Sokolov <dmitriy88.rus@gmail.com>
2022-02-17 16:46:41 +0300

    add lesson7

---
+++ lesson7/src/Task717/Main.java
@@ -0,0 +1,13 @@
+package Task717;
+
+import java.util.function.UnaryOperator;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public UnaryOperator<Integer> sqrt() {
+        return x -> x * x;
+    }
+
+}
---
+++ lesson7/src/Test.java
@@ -0,0 +1,11 @@
+import java.util.function.*;
+
+public class Test {
+    public static void main(String[] args) {
+        DoubleConsumer test;
+        DoubleSupplier t2;
+        DoublePredicate t3;
+        DoubleFunction t4;
+        LongToIntFunction t5;
+    }
+}
