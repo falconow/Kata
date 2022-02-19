99032eaaea3b4bdc2b54358ffb5ac94179bdec77
Dmitry Sokolov <dmitriy88.rus@gmail.com>
2022-02-19 16:27:05 +0300

    add solution lesson7

--- lesson7/src/Task7213/AbstractMail.java
+++ lesson7/src/Task7213/AbstractMail.java
@@ -1,28 +1,28 @@
 package Task7213;
 
-public class AbstractMail<T> implements Sendable<T> {
-    private String From;
-    private String To;
-    private T Content;
+public abstract class AbstractMail<T> implements Sendable<T> {
+    private String from;
+    private String to;
+    private T content;
 
     public AbstractMail (String From, String To, T Content) {
-        this.From = From;
-        this.To = To;
-        this.Content = Content;
+        this.from = From;
+        this.to = To;
+        this.content = Content;
     }
 
     @Override
     public String getFrom() {
-        return From;
+        return from;
     }
 
     @Override
     public String getTo() {
-        return To;
+        return to;
     }
 
     @Override
     public T getContent() {
-        return Content;
+        return content;
     }
 }
